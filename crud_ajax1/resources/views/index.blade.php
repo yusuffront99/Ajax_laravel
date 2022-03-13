794d1211ec21803d6eac3aec3930a2e340ca6af8
yusuf <yusuffront@gmail.com>
2022-03-13 20:41:55 +0800

    created new project ajax crud--

---
+++ crud_ajax1/resources/views/index.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JQUERY</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+</head>
+<body>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12 m-2">
+                <h1>CRUD AJAX</h1>
+                <a class="btn btn-primary btn-sm m-2" id="create"><i class="bi bi-person-plus-fill"></i> create</a>
+                <table class="table table-dark table-hover" id="table-user">
+                    <thead>
+                        <tr>
+                            <th>#ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+{{-- MODAL --}}
+
+<div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+<div class="modal-dialog">
+    <div class="modal-content">
+    <div class="modal-header">
+        <h5 class="modal-title" id="modalLabel"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+        <form name="userForm" id="userForm">
+            <input type="hidden" name="id" id="id">
+            <div class="form-group">
+                <label for="name" class="col-sm-2">Name</label>
+                <input class="form-control" type="text" name="name" id="name" placeholder="your name" required>
+            </div>
+            <div class="form-group">
+                <label for="email" class="col-sm-2">email</label>
+                <input class="form-control" type="email" name="email" id="email" placeholder="your email" required>
+            </div>
+        </form>
+    </div>
+    <div class="modal-footer">
+        <button type="submit" class="btn btn-primary" id="save" value="create">Save</button>
+    </div>
+    </div>
+</div>
+</div>
+{{--  --}}
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
+    {{-- jquery --}}
+    <script>
+        $(function(){
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            var table = $('#table-user').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{route('user.index')}}",
+            columns: [
+                {data: 'id', name:'id'},
+                {data: 'name', name:'name'},
+                {data: 'email', name:'email'},
+                {data: 'action', name:'action'},
+            ]
+        });
+
+        //=== show modal
+        $('#create').click(function(){
+            $('#id').val('');
+            $('#userForm').trigger('reset');
+            $('#modalLabel').html('Add User');
+            $('#modal').modal('show');
+        });
+
+        //=== add user
+        $('#save').click(function(e){
+            e.preventDefault();
+            $(this).html('sending...');
+
+            $.ajax({
+                data: $('#userForm').serialize(),
+                url: "{{route('user.store')}}",
+                type: "POST",
+                dataType: 'json',
+                success: function(data){
+                    console.log('success');
+                },
+                error: function(data) {
+                    console.error('error :', data);
+                }
+            })
+            
+            })
+        });
+    </script>
+</html>
--- crud_ajax1/routes/web.php
+++ crud_ajax1/routes/web.php
@@ -14,4 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AuthController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('user', AuthController::class);
